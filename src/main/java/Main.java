6af200235295d140348988bcd05a4efc80b21629
Krapivka <a.krapivka@helsi.me>
2020-02-14 10:16:42 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+        static class HelloWorld {
+            public static void main(String[] args) {
+                System.out.println("Hello World!");
+            }
+        }
+    }
---
+++ src/main/java/event.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+import org.junit.*;
+
+public class event {
+
+ /*   @Test
+    public void userCanLoginByUsername() {
+        open("/login");
+        $(By.name("user.name")).setValue("johny");
+        $("#submit").click();
+        $(".loading_progress").should(disappear); // Само подождёт, пока элемент исчезнет
+        $("#username").shouldHave(text("Hello, Johny!")); // Само подождёт, пока у элемента появится нужный текст
+
+  */
+
+ @Test
+    public void event (){
+     open("https://qa4.helsi.pro/");
+ }
+
+
+    }
